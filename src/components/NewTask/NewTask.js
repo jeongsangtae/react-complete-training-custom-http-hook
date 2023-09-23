5c3893aecc51ffb6975a624b9027dde349860153
jeongsangtae <wjdtkdxo55@naver.com>
2023-09-23 16:21:21 +0900

    React Complete Training Custom HTTP Hook - More components Custom HTTP Hook use (NewTask component Custom HTTP Hook second way add)

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -5,26 +5,24 @@ import useHttp from "../../hooks/use-http";
 const NewTask = (props) => {
   const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
   const enterTaskHandler = async (taskText) => {
-    const createTask = (taskData) => {
-      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    };
-
     sendTaskRequest(
       {
         url: "https://react-custom-http-hook-e1eb8-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-      },
-      {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: { text: taskText },
       },
-      createTask
+      createTask.bind(null, taskText)
     );
   };
 
